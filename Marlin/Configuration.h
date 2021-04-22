0607c3476f714bd438e3818d47fbe005722b56a9
Vladislav <zhirov0vlad@gmail.com>
2021-04-22 19:50:07 +0300

    added changes from MR https://github.com/MarlinFirmware/Marlin/pull/18423/files

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -150,7 +150,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Kozzlik v1.0.1"
+#define CUSTOM_MACHINE_NAME "Kozzlik v1.0.2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
